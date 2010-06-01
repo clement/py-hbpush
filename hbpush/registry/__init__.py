d9244851c44fba2c719849d13b7ce1e0316ae98e
Clément <clement.nodet@gmail.com>
2010-06-01 22:00:12 +0200

    removed unneeded files

--- hbpush/registry/__init__.py
+++
@@ -1,6 +0,0 @@
-class BaseRegistry(object):
-    def __init__(self, channel_cls):
-        self.channel_cls = channel_cls
-
-    def get_channel(self, name):
-        raise NotImplementedError("")
--- hbpush/registry/memory.py
+++
@@ -1,9 +0,0 @@
-from hbpush.registry import BaseRegistry
-
-class MemoryRegistry(BaseRegistry):
-    def __init__(self, *args, **kwargs):
-        super(MemoryRegistry, self).__init__(*args, **kwargs)
-        self.channels = {}
-
-    def get_channel(self, name):
-        return setdefault(self.channels, name, self.channel_cls())
