7458d07ad26d108b0bce94a1a967b5b031a543b0
Clément <clement.nodet@gmail.com>
2010-06-08 00:13:26 +0200

    nasty race condition when channels are loaded from the store

--- hbpush/channel/memory.py
+++ hbpush/channel/memory.py
@@ -7,53 +7,63 @@ class MemoryChannelRegistry(ChannelRegistry):
     def __init__(self, *args, **kwargs):
         super(MemoryChannelRegistry, self).__init__(*args, **kwargs)
         self.channels = {}
+        self.get_request = {}
 
     def get(self, id, callback, errback):
         try:
             callback(self.channels[id])
         except KeyError:
-            def _cb(message):
-                channel = self.channel_cls(id, self.store)
-                channel.last_message = message
-                self.channels[id] = channel
-                callback(channel)
-            def _eb(error):
-                if error.__class__ == Message.DoesNotExist:
-                    error = Channel.DoesNotExist()
-                errback(error)
-            self.store.get_last(id, _cb, _eb)
-
-
-    def create(self, id, callback, errback, overwrite=False):
-        def _create():
-            channel = self.channel_cls(id, self.store)
-            self.channels[id] = channel
-            callback(channel)
-
-        def _cb(channel):
-            if not overwrite:
-                errback(Channel.Duplicate())
+            # Try to get the channel from the store to populate
+            # the cache.
+            # We might not be the first ones to do that
+            reqs = self.get_request.get(id, ())
+            if len(reqs):
+                self.get_request[id].append((callback, errback))
             else:
-                _create()
+                def _cache_channel(message):
+                    channel = self.channel_cls(id, self.store)
+                    channel.last_message = message
+                    self.channels[id] = channel
+                    for (cb, _) in self.get_request[id]:
+                        cb(channel)
+                    del self.get_request[id]
+                def _no_message(error):
+                    if error.__class__ == Message.DoesNotExist:
+                        error = Channel.DoesNotExist()
+                    for (_, eb) in self.get_request[id]:
+                        eb(error)
+                    del self.get_request[id]
 
-        def _eb(error):
-            # A good thing, we actually need an error
+                self.get_request[id] = [(callback, errback),]
+                self.store.get_last(id, _cache_channel, _no_message)
+
+
+    def create(self, id, callback, errback):
+        def _no_channel(error):
             if error.__class__ == Channel.DoesNotExist:
-                _create()
+                # someone might have populated the cache in between
+                if id in self.channels:
+                    errback(Channel.Duplicate())
+                else:
+                    channel = self.channel_cls(id, self.store)
+                    self.channels[id] = channel
+                    callback(channel)
             else:
                 errback(error)
 
-        self.get(id, _cb, _eb)
+        def _got_channel(channel):
+            errback(Channel.Duplicate())
+
+        self.get(id, _got_channel, _no_channel)
 
 
     def get_or_create(self, id, callback, errback):
-        def _eb(error):
-            if error.__class__ == Channel.DoesNotExist:
-                self.create(id, callback, errback)
+        def _duplicate(error):
+            if error.__class__ == Channel.Duplicate:
+                callback(self.channels[id])
             else:
                 errback(error)
-
-        self.get(id, callback, _eb)
+        self.create(id, callback, _duplicate)
 
 
     def delete(self, id, callback, errback):
